c064ec9405596a5afe036ec735c01d8010f3aab5
Veronika Dzerakh <veronika.dzerakh@tulip.com>
2020-06-04 12:57:56 +0300

    changed serviceController

--- controllers/serviceController.js
+++ controllers/serviceController.js
@@ -1,24 +1,21 @@
-// Import service model
 const Service = require('../models/serviceModel')
 const v4 = require('uuid/v4')
 
-// Handle index actions
-exports.index = function (req, res) {
+exports.index = async function (req, res) {
   try {
-    Service.find({ is_deleted: 0 }, function (err, services) {
-      res.json({
-        status: 'success',
-        message: 'Services retrieved successfully',
-        data: services
-      })
+    const services = await Service.find({ is_deleted: 0 })
+
+    res.json({
+      status: 'success',
+      message: 'Services retrieved successfully',
+      data: services
     })
   } catch (error) {
     res.json({ success: false, message: error })
   }
 }
 
-// Handle create service actions
-exports.new = function (req, res) {
+exports.new = async function (req, res) {
   const object = {
     _id: v4(),
     name: req.body.name ? req.body.name : 'Unknown',
@@ -27,95 +24,86 @@ exports.new = function (req, res) {
     is_deleted: req.body.is_deleted ? req.body.is_deleted : 0
   }
   try {
-    Service.create(object, (error, res) => {
-      res.json({
-        message: 'New service is created!',
-        data: object
-      })
+    await Service.create(object)
+
+    res.json({
+      message: 'New service is created!',
+      data: object
     })
   } catch (error) {
     res.json({ success: false, message: error })
   }
 }
 
-// Handle view service info
-exports.view = function (req, res) {
+exports.view = async function (req, res) {
   try {
-    Service.findById(req.params.service_id, function (err, service) {
-      if (err) { res.send(err) }
-      res.json({
-        message: 'Service details are loaded',
-        data: service
-      })
+    const service = await Service.findById(req.params.service_id)
+
+    res.json({
+      message: 'Service details are loaded',
+      data: service
     })
   } catch (error) {
     res.json({ success: false, message: error })
   }
 }
 
-// Handle update service info
-exports.update = function (req, res) {
+exports.update = async function (req, res) {
   try {
-    Service.findById(req.params.service_id, function (err, service) {
-      service.name = req.body.name
-      service.image = req.body.image
-      service.is_deleted = req.body.is_deleted ? req.body.is_deleted : 0
-      service.date_updated = new Date()
+    const service = await Service.findById(req.params.service_id)
+    service.name = req.body.name
+    service.image = req.body.image
+    service.is_deleted = req.body.is_deleted ? req.body.is_deleted : 0
+    service.date_updated = new Date()
+    service.save()
 
-      service.save(function () {
-        res.json({
-          message: 'Service Info updated',
-          data: service
-        })
-      })
+    res.json({
+      message: 'Service Info updated',
+      data: service
     })
   } catch (error) {
     res.json({ success: false, message: error })
   }
 }
 
-// Handle delete service
-exports.delete = function (req, res) {
+exports.delete = async function (req, res) {
   try {
-    Service.findById(req.params.service_id, function (err, service) {
-      service.is_deleted = 1
-      if (err) { res.json(err) }
-      // eslint-disable-next-line handle-callback-err
-      service.save(function (err) {
-        res.json({
-          success: true,
-          message: 'Service is deleted',
-          id: service._id
-        })
-      })
+    const service = await Service.findById(req.params.service_id)
+    service.is_deleted = 1
+    service.save()
+
+    res.json({
+      success: true,
+      message: 'Service is deleted',
+      id: service._id
     })
   } catch (error) {
     res.json({ success: false, message: error })
   }
 }
 
-exports.getTrash = function (req, res) {
+exports.getTrash = async function (req, res) {
   try {
-    Service.find({ is_deleted: 1 }, function (error, services) {
-      res.json({
-        status: 'success',
-        message: 'Trash is retrieved successfully',
-        data: services
-      })
+    const trash = await Service.find({ is_deleted: 1 })
+
+    res.json({
+      status: 'success',
+      message: 'Trash is retrieved successfully',
+      data: trash
     })
   } catch (error) {
     res.json({ success: false, message: error })
   }
 }
 
-exports.cleanTrash = function (req, res) {
+exports.cleanTrash = async function (req, res) {
   try {
-    Service.deleteMany({ is_deleted: 1 }, function (error, services) {
-      res.json({
-        status: 'success',
-        message: 'Trash is cleared successfully',
-        data: services
-      })
+    const trash = await Service.deleteMany({ is_deleted: 1 })
+
+    res.json({
+      status: 'success',
+      message: 'Trash is cleared successfully',
+      data: trash
     })
   } catch (error) {
     res.json({ success: false, message: error })
